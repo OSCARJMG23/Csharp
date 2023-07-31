6e9737645a6ec69eed0ff4abe67ab0d1a37d48a4
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-07-31 17:25:57 -0500

    new exercise

---
+++ ejercicioList2/Program.cs
@@ -0,0 +1,14 @@
+﻿string ? palabra;
+List<string> Dinosaurios = new List<string>();
+List<string> Encontrados = new List<string>();
+string[] Dinos = {  "Brachioasauruss",
+                    "Amargasaurus",
+                    "Mamenchisaurus"};
+
+Dinosaurios.AddRange(Dinos);
+Console.WriteLine("Ingrese la letra que desea buscar en los dinosaurios: ");
+palabra = Console.ReadLine();
+Encontrados = Dinosaurios.FindAll(n=> n.Contains(palabra ?? String.Empty));
+foreach (String n in Encontrados){
+    Console.WriteLine(n);
+}
---
+++ ejerciciosListas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+
+List<string> Materias = new List<string>();
+Materias.Add("Calculo");
+Materias.Add("Español");
+Materias.Add("Dibujo Tecnico");
+Materias.Add("Ingles");
+
+Console.WriteLine("Total de elementos de la lista {0}", Materias.Count());
+Console.WriteLine("Capacidad Total: {0}", Materias.Capacity);
+Console.WriteLine("Materia[3]: {0}", Materias[4 -1]);
+
+
+string ? palabra;
+List<string> Dinosaurios = new List<string>();
+string[] Dinos = {  "Brachioasauruss",
+                    "Amargasaurus",
+                    "Mamenchisaurus"};
+
+Dinosaurios.AddRange(Dinos);
+Console.WriteLine("Ingrese el nombre del dinosaurio a buscar: ");
+palabra = Console.ReadLine();
+Console.WriteLine(Dinosaurios.Exists(item => item.Equals(palabra)) ? "Se encontro el dinosaurio" : "No se encontro el Dinosaurio");
